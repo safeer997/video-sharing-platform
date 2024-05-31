ec7e343aca4332f4e7f228c46d5367b87e23250d
Safeer <safeeralam997@gmail.com>
2024-05-31 17:52:23 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+
 const generateAccessAndRefreshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -250,5 +251,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     throw new ApiError(401,error?.message||"Invalid refresh token")
   }
 });
+
+
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
